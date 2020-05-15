91e375d2ed9d7ec8db2895afe468bf8a386569d1
Snoopyjoy <hel13140302@126.com>
2020-05-15 15:48:28 +0800

    WIP: lock funcs

---
+++ README.md
@@ -0,0 +1,3 @@
+# A distributed lock base on redis
+## Usage
+Choose a redis client you like and write an implementation of `IRedisClient`.
--- lock.go
+++ lock.go
@@ -11,6 +11,7 @@ import (
 	"time"
 )
 
+// defaultTimeout default lock release time.
 const defaultTimeout = 5 * time.Minute
 const unlockScript = `if redis.call("get", KEYS[1]) == ARGV[1] then return redis.call("del", KEYS[1]) else return 0 end`
 const extendScript = `if redis.call("get", KEYS[1]) == ARGV[1] then return redis.call("pexpire", KEYS[1], ARGV[2]) else return 0 end`
@@ -56,19 +57,25 @@ type ILockFactory interface {
 	GetLock(ctx context.Context, resourceID string) (ILock, error)
 }
 
-type LockFactory struct {
+type lockFactory struct {
 	redisClient IRedisClient
-	lockPool    sync.Pool
+	lockPool    *sync.Pool
 	idGen       func() string
 }
 
+// FactoryOptions custom params to replace default value
 type FactoryOptions struct {
-	IdGenerator func() string
+	// IDGenerator a function generate unique id for each lock, if not
+	IDGenerator func() string
+	// DefaultTimeout default lock release time, if not passed `DefaultTimeout` will be 5 minutes.
+	DefaultTimeout time.Duration
 }
 
 // ILock Lock client interface.
 type ILock interface {
+	// Lock lock a resource, if failed will wait until success or reach the max times.
 	Lock(ctx context.Context)
+	// Realease release lock if lock id is matched.
 	Release(ctx context.Context)
 	// TryLock try to lock resource once.
 	TryLock(ctx context.Context)
@@ -78,27 +85,31 @@ type ILock interface {
 	TTL(ctx context.Context)
 }
 
+// NewLockFactory generate a lock factory.
 func NewLockFactory(ctx context.Context, redisClient IRedisClient, options *FactoryOptions) (ILockFactory, error) {
-
 	if redisClient == nil {
 		panic(errors.New("redislock: no redis client"))
 	}
-	lockPool := sync.Pool{
+	lockPool := &sync.Pool{
 		New: func() interface{} {
 			return &Lock{
 				redisClient: redisClient,
 			}
 		},
 	}
-	ret := LockFactory{
+	ret := lockFactory{
 		redisClient: redisClient,
 		lockPool:    lockPool,
+	}
+
+	if options.IDGenerator != nil {
+		ret.idGen = options.IDGenerator
 	}
 	return &ret, nil
 }
 
 // GetLock Lock instance.
-func (factory *LockFactory) GetLock(ctx context.Context, resourceID string) (ILock, error) {
+func (factory *lockFactory) GetLock(ctx context.Context, resourceID string) (ILock, error) {
 	lock := factory.lockPool.Get().(*Lock)
 	lock.id = factory.idGen()
 	return lock, nil
